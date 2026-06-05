6f5f00d2427db7218d33133a6ea6441704919e06
IagoPL <iagoprietolamas@gmail.com>
2026-06-05 13:03:43 +0200

    Merge nutrition migration branches

---
+++ backend/nutrition/migrations/0030_merge_community_posts_and_equivalence_categories.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('nutrition', '0027_expand_community_posts'),
+        ('nutrition', '0029_fix_equivalencecategory_id_bigint'),
+    ]
+
+    operations = []
